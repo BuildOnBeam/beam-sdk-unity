4baedce3681f253d041f0c264724d2d53a19ef56
Maciej Lewiński <maciej@meritcircle.io>
2025-04-16 09:47:53 +0200

    Feat/optional entity ids (#19)
    * Feat/optional entity ids (#17)
    
    * chore: update http client, make entityId optional when connecting to game
    
    * feat: handle no entityId approach with sessions
    
    * fixup! chore: update http client, make entityId optional when connecting to game
    
    * chore: update version
    
    * chore: update http client, make entityId optional when connecting to game
    
    * feat: handle no entityId approach with sessions
    
    * fixup! chore: update http client, make entityId optional when connecting to game
    
    * chore: update README

--- Unity/Runtime/Scripts/Beam/Models/BeamSession.cs
+++ Unity/Runtime/Scripts/Beam/Models/BeamSession.cs
@@ -6,6 +6,7 @@ namespace Beam.Models
     public class BeamSession
     {
         [JsonProperty("id")] public string Id { get; set; }
+        [JsonProperty("entityId")] public string EntityId { get; set; }
         [JsonProperty("sessionAddress")] public string SessionAddress { get; set; }
         [JsonProperty("startTime")] public DateTimeOffset StartTime { get; set; }
         [JsonProperty("endTime")] public DateTimeOffset EndTime { get; set; }
